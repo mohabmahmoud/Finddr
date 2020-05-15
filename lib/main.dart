d7d3448633fd50d787a9a787cc4558e6efcedeb5
mohabmahmoud <mahmoudmohab25@gmail.com>
2020-05-16 01:38:16 +0200

    new

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(appBar: AppBar(),body: Container(),);
   }
 }
 
