3fd1fff2f55277b8f28608c720e5185e72951d76
mohabmahmoud <mahmoudmohab25@gmail.com>
2020-05-16 02:17:14 +0200

    vvvvc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'styles/apptheme.dart';
 import 'ui/SharedScreen/Splash.dart';
+import 'dart:async';
 
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner:false,theme:appTheme.apptheme ,home: Splash(),));
