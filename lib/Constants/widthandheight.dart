72fc1ee4079b9c311ad2e271cb58969232e3d1c3
mohabmahmoud <mahmoudmohab25@gmail.com>
2020-05-16 02:15:18 +0200

    vvvv

---
+++ lib/Constants/widthandheight.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+ getheight(BuildContext context){
+
+  return MediaQuery.of(context).size.height;
+
+}
+getwidth(BuildContext context){
+   return MediaQuery.of(context).size.width;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
+import 'styles/apptheme.dart';
+import 'ui/SharedScreen/Splash.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner:false,theme:appTheme.apptheme ,home: Splash(),));
 }
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('moah'),),body: Container(),);
-  }
-}
-
